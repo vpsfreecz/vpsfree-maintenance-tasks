fe37b95f1f60942464084620d6d7e80cb9f24d2a
Jakub Skokan <jakub.skokan@havefun.cz>
2018-10-19 14:37:34 +0200

    Add enable-backup-plan

---
+++ 2018-10-19-enable-backup-plan/enable_backup_plan.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+# Enable dataset plan on datasets on selected pool.
+
+POOL_ID = 26
+PLAN = :daily_backup
+
+Dir.chdir('/opt/vpsadmin/api')
+require '/opt/vpsadmin/api/lib/vpsadmin'
+
+module TransactionChains
+  module Maintenance
+    remove_const(:Custom)
+
+    class Custom < TransactionChain
+      label 'Dataset plans'
+
+      def link_chain
+        ::DatasetInPool.where(pool_id: POOL_ID).each do |dip|
+          plan = dip.dataset_in_pool_plans.joins(environment_dataset_plan: :dataset_plan).where(
+            dataset_plans: {name: PLAN.to_s},
+          )
+          
+          if plan.exists?
+            puts "Exists #{dip.pool.node.domain_name}:#{dip.pool.filesystem}/#{dip.dataset.full_name}"
+            next
+          end
+
+          puts "Register #{dip.pool.node.domain_name}:#{dip.pool.filesystem}/#{dip.dataset.full_name}"
+          VpsAdmin::API::DatasetPlans.plans[PLAN].register(dip)
+        end
+      end
+    end
+  end
+end
+
+TransactionChains::Maintenance::Custom.fire
