72a0f760a302f97c327cb103aabbedbb7c6f6fea
Jakub Skokan <jakub.skokan@havefun.cz>
2020-08-11 16:34:01 +0200

    Add script to set memlock prlimit on all VPS

---
+++ 2020-08-11-set-memlock-prlimit/set_memlock_prlimit.rb
@@ -0,0 +1,28 @@
+#!/run/nodectl/nodectl script
+# Set memlock prlimit to unlimited
+require 'nodectld/standalone'
+
+include OsCtl::Lib::Utils::Log
+include NodeCtld::Utils::System
+include NodeCtld::Utils::OsCtl
+
+db = NodeCtld::Db.new
+db.prepared("
+  SELECT v.id
+  FROM vpses v
+  WHERE
+    AND v.object_state < 2
+    AND v.node_id = ?
+", $CFG.get(:vpsadmin, :node_id)).each do |row|
+  puts "VPS #{row['id']}"
+  #next
+
+  begin
+    osctl(
+      %i(ct prlimits set),
+      [row['id'], 'memlock', '65536', '9223372036854775807']
+    )
+  rescue OsCtl::Lib::Exceptions::SystemCommandFailed
+    puts "  -> failed, continue"
+  end
+end
