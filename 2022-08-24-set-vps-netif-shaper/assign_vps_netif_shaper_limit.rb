7ffebb5af6f87e26441cad897b90dce7ccc0432c
Jakub Skokan <jakub.skokan@havefun.cz>
2022-09-01 11:04:51 +0200

    set-vps-netif-shaper: parametrize, preserve custom limits

--- 2022-08-24-set-vps-netif-shaper/assign_vps_netif_shaper_limit.rb
+++ 2022-08-24-set-vps-netif-shaper/assign_vps_netif_shaper_limit.rb
@@ -1,4 +1,7 @@
 #!/run/current-system/sw/bin/vpsadmin-api-ruby
+# Usage: $0 <node id> [execute]
+#
+# Description: set network interface shaper on the selected node
 require 'vpsadmin'
 
 module TransactionChains
@@ -8,9 +11,9 @@ module TransactionChains
     class Custom < TransactionChain
       label 'Shaper'
 
-      def link_chain
+      def link_chain(node_id, execute_it)
         ::Vps.where(
-          node_id: 400,
+          node_id: node_id,
           object_state: [
             ::Vps.object_states[:active],
             ::Vps.object_states[:suspended],
@@ -20,17 +23,36 @@ module TransactionChains
           vps.network_interfaces.each do |netif|
             puts "VPS #{vps.id} #{netif.name}"
 
-            use_chain(TransactionChains::NetworkInterface::Update, args: [netif, {
-              max_tx: 300 * 1024 * 1024,
-              max_rx: 300 * 1024 * 1024,
-            }])
+            max_tx_ip = netif.ip_addresses.order('max_tx DESC').take
+            max_tx = max_tx_ip ? max_tx_ip.max_tx * 8 : 300 * 1024 * 1024
+
+            max_rx_ip = netif.ip_addresses.order('max_rx DESC').take
+            max_rx = max_rx_ip ? max_rx_ip.max_rx * 8 : 300 * 1024 * 1024
+
+            puts "  TX = #{(max_tx / 1024.0 / 1024).round} Mbps"
+            puts "  RX = #{(max_rx / 1024.0 / 1024).round} Mbps"
+            puts
+
+            use_chain(
+              TransactionChains::NetworkInterface::Update,
+              args: [
+                netif,
+                {max_tx: max_tx, max_rx: max_rx},
+              ],
+              reversible: :keep_going,
+            )
           end
         end
 
-        fail 'not yet bro'
+        fail 'not yet bro' unless execute_it
       end
     end
   end
 end
 
-TransactionChains::Maintenance::Custom.fire
+if ARGV.length < 1
+  warn "Usage: #{$0} <node id> [execute]"
+  exit(false)
+end
+
+TransactionChains::Maintenance::Custom.fire(ARGV[0].to_i, ARGV[1] == 'execute')
