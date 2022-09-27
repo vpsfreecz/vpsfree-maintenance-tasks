8733df887ed232ac35054b8763b14f40d8083646
Jakub Skokan <jakub.skokan@havefun.cz>
2022-09-27 17:53:09 +0200

    switch-to-manual-ksoftlimd: add sleep after disabling system-wide

--- 2022-09-27-switch-to-manual-ksoftlimd/switch-to-manual-ksoftlimd.sh
+++ 2022-09-27-switch-to-manual-ksoftlimd/switch-to-manual-ksoftlimd.sh
@@ -2,6 +2,8 @@
 
 echo 0 > /proc/sys/vm/cgroup_memory_ksoftlimd_for_all
 
+sleep 10
+
 for controlfile in /sys/fs/cgroup/memory/osctl/pool.tank/group.default/user.*/ct.*/memory.ksoftlimd_control ; do
         echo $controlfile
         echo 1 > $controlfile
